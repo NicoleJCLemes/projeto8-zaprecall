a2480f392857dd6bd2ae6ebf07dff10054630376
Guga <contato.gustavobarbosa@outlook.com>
2022-03-23 16:02:35 -0300

    Preparando revisão de código

--- src/CardQuestion.js
+++
@@ -1,8 +0,0 @@
-export default function CardQuestion(props){
-    return(
-        <div className="cardQuestion">
-            <p>{props.question}</p>
-            <img onClick={() => props.callback('questionAnswer')} src="./assets/setinha.png" alt="flip arrow"/>
-        </div>
-    )
-}
